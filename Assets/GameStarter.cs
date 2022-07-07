931b687122500f820f0906c4b9f57c135983e28b
SolarBlaze <veselovrv64@gmail.com>
2022-07-07 13:58:27 +0300

    [ADD] GameStarter

---
+++ Assets/GameStarter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Diabloid
+{
+    public class GameStarter : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+
+        private void Awake()
+        {
+
+        }
+    }
+}
